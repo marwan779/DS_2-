5132473c99c020105f557501a2124bd646b87568
marwan mohamed <ppgming@gmail.com>
2024-12-24 16:49:16 +0200

    first commit

---
+++ DoubleLinkedList.h
@@ -0,0 +1,63 @@
+/*
+                    Documentation Part
+===================================================================
+    @file           : DoubleLinkedList.h
+    @brief          : Contains the Structure Of Nodes & 
+                      Prototypes of the Double Linked List 
+                      functions DS
+===================================================================
+
+*/
+
+/*====================== Start Of File Gaurd ======================*/
+
+#ifndef DOUBLELINKEDLIST_H
+#define DOUBLELINKEDLIST_H
+
+/*====================== Includes ======================*/
+
+#include <iostream>
+#include "Book.h"
+using namespace std;
+
+struct Node
+{
+    Book Data;
+    Node *Next;
+    Node *Back;
+};
+
+/*================ Prototypes Of Functions ================*/
+
+
+class DoubleLinkedList
+{
+public:
+    DoubleLinkedList();
+    void InsertAtBegnning(const Book &Item);
+    Book Search(const string Title);
+    int Lenght();
+    void DeleteNode(const int Location);
+    void PrintForward();
+    void PrintByCategory(string Category);
+    void PrintBook(Book book);
+    void Free();
+    bool BookExist(const string Title);
+    void Sort();
+    bool IsEmpty();
+    string Error;
+
+
+protected:
+    Node *Head;
+    Node *Tail;
+    int Count;
+
+        
+
+};
+
+
+/*====================== End Of File Gaurd ======================*/
+
+#endif
